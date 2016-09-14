2decf95d8d63bddd70826a35c64589452183fab8
agdavid <antonio.g.david@gmail.com>
2016-09-14 08:05:11 -0400

    add JS associative array and HTML markup to display

---
+++ js/app.js
@@ -0,0 +1,13 @@
+var myArray = []
+myArray["name"] = "Matz";
+myArray["language"] = "Ruby";
+myArray["country"] = "Japan";
+
+function displayArray() {
+  var ResultsDiv = document.getElementById('resultsDiv');
+  for(k in myArray) {
+    var ChildDiv = document.createElement('div');
+    ChildDiv.innerHTML = myArray[k];
+    ResultsDiv.appendChild(ChildDiv);
+  }
+};
